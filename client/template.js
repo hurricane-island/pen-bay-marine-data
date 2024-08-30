1521af449d5f804c1dbfbf557e184ed58791ebad
Keeney <nkeeney@oceanics.io>
2024-08-30 17:46:00 -0400

    md5 program files

--- client/template.js
+++ client/template.js
@@ -1,17 +1,22 @@
-import {readFileSync, writeFileSync, mkdirSync} from "fs";
+import {readFileSync, writeFileSync, mkdirSync, existsSync} from "fs";
 import Crypto from "crypto-js";
 const {MD5} = Crypto;
 
-const template = "../logger-programs/default.template.dld";
-const station = "Ai1_36594";
-const pakbus = 1065;
-const directory = `../logger-programs/${station}`;
+const template = "../logger-programs/test.template.dld";
+const config = [["Ai1_36594", 1065], ["Ai1_36593", 1064], ["Ai1_36592", 1062]];
+const text = readFileSync(template, "utf8");
 
-const text = readFileSync(template, "utf8")
-    .replace("$TEMPLATE_STATION_NAME", station)
-    .replace("$TEMPLATE_PAKBUS_ADDRESS", `${pakbus}`);
-const checksum = MD5(text).toString();
-console.log({checksum})
-mkdirSync(directory)
-const program = `${directory}/default.dld`;
-writeFileSync(program, text, "utf8");
+function station(name, address) {
+    const directory = `../logger-programs/${name}`;
+    const content = text
+        .replace("$TEMPLATE_STATION_NAME", name)
+        .replace("$TEMPLATE_PAKBUS_ADDRESS", `${address}`);
+    const checksum = MD5(content).toString();
+    if (!existsSync(directory)) mkdirSync(directory)
+    const program = `${directory}/test.${checksum}.dld`;
+    writeFileSync(program, content, "utf8");
+}
+
+config.forEach(([name, address])=>{
+    station(name, address)
+})
