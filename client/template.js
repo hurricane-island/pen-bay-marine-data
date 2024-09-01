371d42195c60e5ee2692bab1fe9aeed0ac2b28bc
Keeney <nkeeney@oceanics.io>
2024-08-31 22:54:54 -0400

    move logger programs

--- client/template.js
+++ client/template.js
@@ -1,22 +1,35 @@
-import {readFileSync, writeFileSync, mkdirSync, existsSync} from "fs";
+import {readFileSync, writeFileSync, mkdirSync, existsSync,} from "fs";
 import Crypto from "crypto-js";
+import {create} from "tar";
 const {MD5} = Crypto;
 
-const template = "../logger-programs/test.template.dld";
-const config = [["Ai1_36594", 1065], ["Ai1_36593", 1064], ["Ai1_36592", 1062]];
+const template = "../logger-programs/template/default.dld";
+const config = [
+    ["Ai1_36594", 1065], 
+    // ["Ai1_36593", 1064], 
+    // ["Ai1_36592", 1062]
+];
 const text = readFileSync(template, "utf8");
 
 function station(name, address) {
     const directory = `../logger-programs/${name}`;
+    if (!existsSync(directory)) mkdirSync(directory)
+
     const content = text
         .replace("$TEMPLATE_STATION_NAME", name)
         .replace("$TEMPLATE_PAKBUS_ADDRESS", `${address}`);
     const checksum = MD5(content).toString();
-    if (!existsSync(directory)) mkdirSync(directory)
-    const program = `${directory}/test.${checksum}.dld`;
+    
+    const program = `${directory}/default.${checksum}.dld`;
     writeFileSync(program, content, "utf8");
+    return create({
+        gzip: true,
+        file: `../logger-programs/${name}.web.obj.tar.gz`
+    }, ["../logger-programs/template/definitions.dld"])
 }
 
 config.forEach(([name, address])=>{
-    station(name, address)
+    (async function () {
+        await station(name, address)
+    })()
 })
