96ed28f789ccd467a079b4eb378568eda7a55335
Keeney <nkeeney@oceanics.io>
2024-09-15 19:09:24 -0400

    cable figure styling ggplot2

--- cable/cable.r
+++ cable/cable.r
@@ -6,4 +6,19 @@ library(ggplot2)
 filename <- "/Users/keeney/pen-bay-marine-data/cable/hurricane-scientific-mooring.mat"
 results <- R.matlab::readMat(filename)
 df <- data.frame(results)
-df |> ggplot(aes(x = x, y = z)) + geom_point(aes(color = T))
+df |> ggplot(aes(x = x, y = z)) + 
+    geom_point(aes(color = T)) + 
+    geom_hline(yintercept=df$depth[0:1], linetype="solid", color = "black") + 
+    geom_hline(yintercept=0, linetype="solid", color = "black") + 
+    geom_hline(yintercept=max(df$z), linetype="dashed", color = "black") +
+    geom_vline(xintercept=max(df$x), linetype="dashed", color = "black") +
+    scale_x_continuous(breaks = seq(0, max(df$x), by = 2)) +
+    scale_y_continuous(breaks = seq(0, df$depth[0:1], by = 2)) +
+    coord_fixed() + 
+    theme_linedraw() +
+    labs(
+        # title = "Mooring static analysis",
+        x = "X (m)",
+        y = "Z (m)",
+        color = "Tension (N)"
+    )
