e53e02ae8969da040aef4621619d4b9f3c7728bb
Keeney <nkeeney@oceanics.io>
2025-04-25 15:37:06 -0400

    QC and Influx not used

--- ioos_qc/main.py
+++
@@ -1,9 +0,0 @@
-from ioos_qc import qartod
-
-results = qartod.gross_range_test(
-    inp=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-    suspect_span=[0, 8],
-    fail_span=[0, 10]
-)
-
-print(results)
