0ad36a15fc80bd6232a9f3a8f635cab1572f53c9
Keeney <nkeeney@oceanics.io>
2025-08-15 16:21:27 -0400

    add downloaded weather data

---
+++ weather/cli.py
@@ -0,0 +1,12 @@
+"""
+Weather CLI
+"""
+
+def hello_world():
+    """
+    A simple function to print 'Hello, World!' to the console.
+    """
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    hello_world()
