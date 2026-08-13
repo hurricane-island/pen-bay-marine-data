1c6dc460e118796dc7defb0b90fe25650075d730
Keeney <nkeeney@oceanics.io>
2026-08-13 12:28:31 -0400

    json fails

--- buoys/worker.js
+++ buoys/worker.js
@@ -52,7 +52,7 @@ export default {
     //   return new Response("Unauthorized", { status: 401 });
     // }
     try {
-      const body = await request.json();
+      const body = await request.text();
       console.log("Received body:", body);
       // const line = parseCRMessage(JSON.parse(body));
       // return await fetch(influxUrl, {
